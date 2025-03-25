e74d164f6017f58d45904b25ac14a4351332b134
Juan Delvalle <acx.jose@fpuna.edu.py>
2025-03-25 19:43:20 -0300

    Agrega informacion a modelos segun DER

--- facturas/models.py
+++ facturas/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 
 from clientes.models import Cliente
+from productos.models import Producto
 
 # Create your models here.
 
 class Factura(models.Model):
-    numero = models.CharField(max_length=80)
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    cod_factura = models.CharField(max_length=20, unique=True)
+    cod_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, to_field='documento')
+    nombre_empleado = models.CharField(max_length=100)
+    total_factura = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.cod_factura
+    
+class DetalleFactura(models.Model):
+    cod_factura = models.ForeignKey(Factura, on_delete=models.CASCADE, to_field='cod_factura')
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
     total = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.numero
+        return f'Detalle de {self.cod_factura} - {self.producto.nombre}'
--- proveedores/models.py
+++ proveedores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+    documento = models.CharField(max_length=20, unique=True, primary_key=True)
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200, blank=True, null=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
