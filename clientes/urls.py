5c7fde6037fbd376efbf80504fd9dfee20f7fd86
Juan Delvalle <acx.jose@fpuna.edu.py>
2025-04-07 14:32:03 -0300

    Agrega escenario de prueba - ventas

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:pk>/show", views.show, name="show"),
     path("<int:pk>/update", views.update, name="update"),
     path("<int:pk>/destroy", views.destroy, name="destroy"),
     
